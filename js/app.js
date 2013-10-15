c0f44b6c23916255cd9194df7e8c10ff1753a4a7
Samim Pezeshki <p.samim@gmail.com>
2013-10-16 01:53:56 +0330

    About page

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ var notFoundTemplate = '<section id="main"><div class="not-found">404</div></sec
  });
 
 function HomeCtrl($scope, $routeParams, $http) {
-	$http.get('/js/posts.json?7', {cache:true}).success( function(data) {
+	$http.get('/js/posts.json?8', {cache:true}).success( function(data) {
 		data.tags.splice(0,1);
 		data.posts.splice(0,1);
 		$scope.tags = data.tags;
