d1346fb5459309df4287a3a30b931e94a7c405e0
Samim Pezeshki <p.samim@gmail.com>
2013-10-20 15:29:59 +0330

    c

--- js/app.js
+++ js/app.js
@@ -85,3 +85,9 @@ function PostCtrl($scope, $routeParams, $http, $sce) {
 	});
 }
 
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-44987148-1', 'psam.im');
+  ga('send', 'pageview');
